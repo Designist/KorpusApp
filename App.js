3afaa874d596fc737a52fdeed631f2ac5527325f
Nicholas Tomlin <Nicholas_Tomlin@brown.edu>
2017-08-08 17:25:25 -0400

    working version

--- App.js
+++ App.js
@@ -2897,68 +2897,65 @@ var data2 = {
   ]
 };
 
-function Row({ numSlots, values, tier }) {
-  // I/P: numSlots, taken from parent sentence
-  //      values, list of segments (e.g., morphemes) with start/end times
-  //      tier, the tier name
-  // O/P: single row of glossed sentence, with colspan spacing
-  // Status: tested, working
-  const finalSlot = numSlots;
-  let currentSlot = 0;
-  let output = [];
+// function Row({ numSlots, values, tier }) {
+//   // I/P: numSlots, taken from parent sentence
+//   //      values, list of segments (e.g., morphemes) with start/end times
+//   //      tier, the tier name
+//   // O/P: single row of glossed sentence, with colspan spacing
+//   // Status: tested, working
+//   const finalSlot = numSlots;
+//   let currentSlot = 0;
+//   let output = [];
 
-  for (const v of values) {
-    const startSlot = v['start_slot'];
-    const endSlot = v['end_slot'];
-    const text = v['value'];
+//   for (const v of values) {
+//     const startSlot = v['start_slot'];
+//     const endSlot = v['end_slot'];
+//     const text = v['value'];
 
-    // Add blank space before current value:
-    if (startSlot > currentSlot) {
-      const diff = String(startSlot - currentSlot);
-      output.push(<td key={id.generate()} colSpan={diff} />);
-    }
-    // Create element with correct 'colSpan' width:
-    const size = String(endSlot - startSlot);
-    output.push(<td key={id.generate()} colSpan={size}>{text}</td>);
-    currentSlot = endSlot;
-  }
-  // Fill blank space at end of table row:
-  if (currentSlot < finalSlot) {
-    const diff = String(finalSlot - currentSlot);
-    output.push(<td key={id.generate()} colSpan={diff} />);
-  }
-  return <tr data-tier={tier}>{output}</tr>;
-}
+//     // Add blank space before current value:
+//     if (startSlot > currentSlot) {
+//       const diff = String(startSlot - currentSlot);
+//       output.push(<td key={id.generate()} colSpan={diff} />);
+//     }
+//     // Create element with correct 'colSpan' width:
+//     const size = String(endSlot - startSlot);
+//     output.push(<td key={id.generate()} colSpan={size}>{text}</td>);
+//     currentSlot = endSlot;
+//   }
+//   // Fill blank space at end of table row:
+//   if (currentSlot < finalSlot) {
+//     const diff = String(finalSlot - currentSlot);
+//     output.push(<td key={id.generate()} colSpan={diff} />);
+//   }
+//   return <tr data-tier={tier}>{output}</tr>;
+// }
 
-function Sentence({ sentence }) {
-  // I/P: sentence, a sentence
-  // O/P: table of glossed Row components
-  // Status: tested, working
-  let rowList = []; // to be output
-  const numSlots = sentence['num_slots'];
-  // Add the indepentent tier, i.e., the top row, to the list of rows. Note that
-  // 'colSpan={numSlots}' ensures that this row spans the entire table.
-  rowList.push(
-    <tr data-tier={sentence['tier']}>
-      <td colSpan={numSlots} className="topRow">{sentence['text']}</td>
-    </tr>
-  );
-  const dependents = sentence['dependents']; // list of dependent tiers, flat structure
-  // Add each dependent tier to the row list:
-  for (const {values, tier} of dependents) {
-    // Tier attribute will be used for hiding/showing tiers:
-    rowList.push(<Row key={id.generate()} numSlots={numSlots} values={values} tier={tier} />);
-  }
-  return <table className="gloss"><tbody>{rowList}</tbody></table>;
-}
+// function Sentence({ sentence }) {
+//   // I/P: sentence, a sentence
+//   // O/P: table of glossed Row components
+//   // Status: tested, working
+//   let rowList = []; // to be output
+//   const numSlots = sentence['num_slots'];
+//   // Add the indepentent tier, i.e., the top row, to the list of rows. Note that
+//   // 'colSpan={numSlots}' ensures that this row spans the entire table.
+//   rowList.push(
+//     <tr data-tier={sentence['tier']}>
+//       <td colSpan={numSlots} className="topRow">{sentence['text']}</td>
+//     </tr>
+//   );
+//   const dependents = sentence['dependents']; // list of dependent tiers, flat structure
+//   // Add each dependent tier to the row list:
+//   for (const {values, tier} of dependents) {
+//     // Tier attribute will be used for hiding/showing tiers:
+//     rowList.push(<Row key={id.generate()} numSlots={numSlots} values={values} tier={tier} />);
+//   }
+//   return <table className="gloss"><tbody>{rowList}</tbody></table>;
+// }
 
 class TextDisplay extends React.Component {
   // I/P: data, a single story in JSON format
   // O/P: a div containing the plaintext of this story
   // Status: tested, not working
-  componentDidMount() {
-    Orientation.lockToLandscape();
-  }
   render() {
     var output = [];
     var data = this.props.data;
@@ -2978,7 +2975,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View>
-        <TextDisplay data={data2}/>
+        <TextDisplay data={data2} />
       </View>
     );
   }
